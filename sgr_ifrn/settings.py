bd1b4c30d157cc3542c65b38c663d9d649264737
Luan Medeiros Macena <luanxmedeiros@live.com>
2016-07-30 02:32:40 -0300

    Configuração do banco postgres

--- sgr_ifrn/settings.py
+++ sgr_ifrn/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'sgr_ifrn.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'sgr',
+        'USER': 'postgres',
+        'PASSWORD': '197352',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
